4380b8cbb5d0e560acb6dbef9e14b586e505f876
Maryline <marybot@free.fr>
2022-01-27 21:24:26 +0100

    update app favorites

--- nutella/favorite/urls.py
+++ nutella/favorite/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.Favorite_view(), name="favorites"),
+    path("favorites/", views.ShowFavorites.as_view(), name="favorites"),
 ]
--- nutella/favorite/views.py
+++ nutella/favorite/views.py
@@ -1,9 +1,22 @@
 from msilib.schema import ListView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from nutella.models import Favorite
+from login.models import User
+from django.contrib import messages
 
 
 class ShowFavorites(ListView):
     template_name = "favorites.html"
-    model = Favorite()
+    model = Favorite
     context_object_name = "favorites"
+
+    user = User()
+
+    if user.is_authenticated == False:
+        messages.add_message(
+            messages.SUCCESS, "Vous devez être connecté pour consulter la page favoris."
+        )
+        redirect("login")
+
+    def delete_favorite(self, id):
+        pass
